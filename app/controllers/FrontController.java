31ae6275ac9aec66c455d284f0ba8fda97d42c1a
Yohann Troude <yohann.troude@gmail.com>
2018-12-07 16:51:33 +0100

    TODO

--- app/controllers/FrontController.java
+++ app/controllers/FrontController.java
@@ -9,13 +9,12 @@ import models.*;
 public class FrontController extends Controller {
 
     public static void index() {
-
         List<Produit> produits = Produit.findAll();
         render(produits);
     }
 
     public static void afficherPanier(){
-
+        //TODO
     }
     public static void detailProduit(String produitUuid){
         Produit produit = Produit.findById(produitUuid);
